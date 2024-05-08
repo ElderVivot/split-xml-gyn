3f1f786e76e12b3b83d7cb860d8b5ed8c6eb7fd7
ElderVivot <eldervivot@gmail.com>
2024-05-08 16:58:00 -0300

    feat: add os system

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os
 from src.process import ProcessXmls
 
 dataJson = {}
@@ -6,3 +7,5 @@ with open('configuracoes.json', 'r', encoding='utf-8') as f:
     dataJson = json.load(f)
 
 ProcessXmls(dataJson['pasta_xml'], dataJson['somente_pasta_com_nome']).process()
+
+os.system('pause > nul')
