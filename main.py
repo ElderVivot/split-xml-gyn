36d3f0e312aae2260786fe09918e4d9986582a9b
ElderVivot <eldervivot@gmail.com>
2024-08-15 22:05:21 -0300

    feat: implement to start option with rocketry

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import json
 import os
 from src.process import ProcessXmls
+from rocketry import Rocketry
 
 dataJson = {}
 with open('configuracoes.json', 'r', encoding='utf-8') as f:
     dataJson = json.load(f)
 
-ProcessXmls(dataJson['pasta_xml'], dataJson['somente_pasta_com_nome']).process()
+appRocketry = Rocketry()
 
-os.system('pause')
+
+@appRocketry.task('every 12 hour', name="process_xml_split")
+def processXmlSplit():
+    ProcessXmls(dataJson['pasta_xml'], dataJson['somente_pasta_com_nome']).process()
+
+
+if __name__ == "__main__":
+    appRocketry.run()
